a45ea348e494a41d11f43dfdf47964b46627246c
TheoR <theo.retif@gmail.com>
2024-02-21 16:12:06 +0100

    commit debug input shapes do not overlap raster

--- Ontario_processing.py
+++ Ontario_processing.py
@@ -47,7 +47,7 @@ def compute_ndvi_and_rao_on_tree_by_id(b04_filepath,
     The tree is selected through its ID
     """
     #Select tree
-    tree = data[data['FMFOBJID'].astype('int') == tree_id].iloc[0]
+    tree = data[data['POLYID'].astype('int') == tree_id].iloc[0]
 
     return compute_ndvi_and_rao_on_tree(
         b04_filepath, b08_filepath, tree, ndvi_threshold, window, na_tolerance,
@@ -99,20 +99,46 @@ if __name__ == '__main__':
     elif 'OSPCOMP' and 'USPCOMP' in data:
         species_keys = ['OSPCOMP', 'USPCOMP']
 
-    print("Columns in data GeoDataFrame:", data.columns)
+   # print("Columns in data GeoDataFrame:", data.columns)
 
     forest = data[data['POLYTYPE'] == 'FOR'].copy()
 
-    ## Change projection to be consistent with Sentinel 2 projection (Ontario)
-    with rasterio.open(b08_filepath) as src:
-        crs_S2 = src.crs
-    forest.to_crs(crs=crs_S2, inplace=True)
+
+    # ## Change projection
+    # with rasterio.open(b08_filepath) as srcS2:
+    #     crs_S2 = srcS2.crs
+    # forest.to_crs(crs=crs_S2, inplace=True)
+    # print("CRS S2 :", crs_S2)
+    # print("CRS FOR :", forest.crs)
+
+    # Change projection
+   # forest.to_crs("EPSG:32617", inplace=True)
+
+    # Change projection
+    new_epsg_code = 32617
+    with rasterio.open(b04_filepath, "r+") as srcB04:
+        srcB04.crs = rasterio.crs.CRS.from_epsg(new_epsg_code)
+    with rasterio.open(b08_filepath, "r+") as srcB08:
+        srcB08.crs = rasterio.crs.CRS.from_epsg(new_epsg_code)
+    with rasterio.open(b04_filepath) as srcB04:
+        print("CRS de B04 après changement :", srcB04.crs)
+    with rasterio.open(b08_filepath) as srcB08:
+        print("CRS de B08 après changement :", srcB08.crs)
+
+    # Simplifier les géométries du GeoDataFrame
+    forest['geometry'] = forest['geometry'].apply(lambda geom: geom.convex_hull)
+
+    # Convertir MultiPolygons en Polygons
+    forest['geometry'] = forest['geometry'].apply(lambda geom: geom.geoms[0] if geom.is_empty else geom)
+
+    forest.to_crs(crs=new_epsg_code, inplace=True)
+    print("CRS forest :", forest.crs)
 
     calib_plots = forest[forest['SOURCE'] == 'PLOTVAR']
 
     # Methodology meta parameters
     window = 3
-    na_tolerance = 0.3
+    na_tolerance = 0.4
     ndvi_threshold = 0.4
 
     results = pd.DataFrame(columns=["ID", "rao", "shannon"])
@@ -124,21 +150,21 @@ if __name__ == '__main__':
                 plot=False
             )
             if np.isnan(np.nanmean(rao)):
-                print(f"Tree {tree['FMFOBJID']} has empty intersection, skipping...")
+                print(f"Tree {tree['POLYID']} has empty intersection, skipping...")
                 continue
             forest = data[data['POLYTYPE'] == 'FOR'].copy()
             forest['Shannon'] = forest[species_keys[0]].map(compute_shannon)
         except ValueError as e:
-            print(f"Tree {tree['FMFOBJID']} not processed")
+            print(f"Tree {tree['POLYID']} not processed")
             print(e)
             continue
         else:
-            print(f"Tree {tree['FMFOBJID']} has been successfully processed")
+            print(f"Tree {tree['POLYID']} has been successfully processed")
             results = pd.concat(
                 [results,
                  pd.DataFrame(
                      [
-                         [tree['FMFOBJID'], np.nanmean(rao), forest['Shannon']]
+                         [tree['POLYID'], np.nanmean(rao), forest['Shannon']]
                      ],
                      columns=results.columns
                  )]
@@ -148,6 +174,30 @@ if __name__ == '__main__':
     plt.figure()
     sns.scatterplot(results, x='shannon', y='rao')
     plt.show()
+
+    ## DEBUG
+
+    # Avant le changement de CRS
+    print("CRS GeoDataFrame avant le changement :", data.crs)
+    print("CRS B04 avant le changement :", rasterio.open(b04_filepath).crs)
+    print("CRS B08 avant le changement :", rasterio.open(b08_filepath).crs)
+
+    # Après le changement de CRS
+    print("CRS GeoDataFrame après le changement :", forest.crs)
+    print("CRS B04 après le changement :", rasterio.open(b04_filepath).crs)
+    print("CRS B08 après le changement :", rasterio.open(b08_filepath).crs)
+
+    # Vérification des coordonnées spatiales des formes géométriques
+    print("Extent GeoDataFrame :", data.total_bounds)
+    print("Extent B04 :", rasterio.open(b04_filepath).bounds)
+    print("Extent B08 :", rasterio.open(b08_filepath).bounds)
+
+    print("Quelques lignes du GeoDataFrame :", forest.head())
+
+    print("Chemin du fichier B04 :", b04_filepath)
+    print("Chemin du fichier B08 :", b08_filepath)
+
+    print("Géométrie de la première ligne du GeoDataFrame :", forest.geometry.iloc[0])
 
     # If we want to check a specific tree. Execute this code
     # selected_tree_id = 165
